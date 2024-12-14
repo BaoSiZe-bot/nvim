7212d6a4186d602897aa7ed164e6d417d41ed0e7
Size Bao <baosize@hotmail.com>
2024-12-14 10:10:04 +0800

    Add smear-cursor.

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -47,210 +47,6 @@ return {
     event = "VeryLazy",
     opts = {},
   },
-  -- {
-  --   "yioneko/nvim-cmp",
-  --   priority = 10000,
-  --   branch = "perf",
-  --   event = "InsertEnter",
-  --   dependencies = {
-  --     "neovim/nvim-lspconfig",
-  --     "hrsh7th/cmp-nvim-lsp",
-  --     "hrsh7th/cmp-path",
-  --     "hrsh7th/cmp-cmdline",
-  --   },
-  --   config = function()
-  --     local kind_icons = {
-  --       Array = "",
-  --       Boolean = "󰨙",
-  --       Class = "",
-  --       Codeium = "󰘦",
-  --       Color = "",
-  --       Control = "",
-  --       Collapsed = "",
-  --       Constant = "󰏿",
-  --       Constructor = "",
-  --       Copilot = "",
-  --       Enum = "",
-  --       EnumMember = "",
-  --       Event = "",
-  --       Field = "",
-  --       File = "",
-  --       Folder = "",
-  --       Function = "󰊕",
-  --       Interface = "",
-  --       Key = "",
-  --       Method = "󰊕",
-  --       Keyword = "",
-  --       Module = "",
-  --       Namespace = "󰦮",
-  --       Null = "",
-  --       Number = "󰎠",
-  --       Object = "",
-  --       Operator = "",
-  --       Package = "",
-  --       Property = "",
-  --       Reference = "",
-  --       Snippet = "",
-  --       String = "",
-  --       Struct = "󰆼",
-  --       TabNine = "󰏚",
-  --       Text = "",
-  --       TypeParameter = "",
-  --       Unit = "",
-  --       Value = "",
-  --       Variable = "󰀫",
-  --     }
-  --     local cmp = require("cmp")
-  --     local compare = require("cmp.config.compare")
-  --     local formatting_style = {
-  --       fields = { "kind", "abbr", "menu" },
-  --       format = function(_, item)
-  --         item.kind = kind_icons[item.kind]
-  --         return item
-  --       end,
-  --     }
-  --     local WIDE_HEIGHT = 20
-  --     local function border(hl_name)
-  --       return {
-  --         { "╭", hl_name },
-  --         { "─", hl_name },
-  --         { "╮", hl_name },
-  --         { "│", hl_name },
-  --         { "╯", hl_name },
-  --         { "─", hl_name },
-  --         { "╰", hl_name },
-  --         { "│", hl_name },
-  --       }
-  --     end
-  --     local select_next_item = function(option)
-  --       return function(fallback)
-  --         if not require("cmp").select_next_item(option) then
-  --           local release = require("cmp").core:suspend()
-  --           fallback()
-  --           vim.schedule(release)
-  --         end
-  --       end
-  --     end
-  --     local select_prev_item = function(option)
-  --       return function(fallback)
-  --         if not require("cmp").select_prev_item(option) then
-  --           local release = require("cmp").core:suspend()
-  --           fallback()
-  --           vim.schedule(release)
-  --         end
-  --       end
-  --     end
-  --     local abort = function()
-  --       return function(fallback)
-  --         if not require("cmp").abort() then
-  --           fallback()
-  --         end
-  --       end
-  --     end
-  --     local types = require("cmp.types")
-  --     cmp.setup({
-  --       matching = {
-  --         disallow_fuzzy_matching = true,
-  --         disallow_fullfuzzy_matching = false,
-  --         disallow_partial_fuzzy_matching = false,
-  --         disallow_partial_matching = false,
-  --         disallow_prefix_unmatching = false,
-  --         disallow_symbol_nonprefix_matching = false,
-  --       },
-  --       performance = {
-  --         debounce = 0,
-  --         throttle = 0,
-  --         fetching_timeout = 0,
-  --         confirm_resolve_timeout = 0,
-  --         async_budget = 1,
-  --         max_view_entries = 500,
-  --       },
-  --       mapping = {
-  --         ["<C-Space>"] = cmp.mapping.complete(),
-  --         ["<cr>"] = LazyVim.cmp.confirm({ select = true }),
-  --         ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
-  --         ["<C-f>"] = {
-  --           i = select_next_item({ behavior = types.cmp.SelectBehavior.Select }),
-  --         },
-  --         ["<C-b>"] = {
-  --           i = select_prev_item({ behavior = types.cmp.SelectBehavior.Select }),
-  --         },
-  --         ["<C-e>"] = {
-  --           i = abort(),
-  --         },
-  --       },
-  --       auto_brackets = {},
-  --       completion = {
-  --         completeopt = "menu,menuone,noinsert",
-  --       },
-  --       preselect = cmp.PreselectMode.Item,
-  --       experimental = {
-  --         ghost_text = {
-  --           hl_group = "CmpGhostText",
-  --         },
-  --       },
-  --       formatting = formatting_style,
-  --       sources = cmp.config.sources({
-  --         { name = "nvim_lsp" },
-  --         { name = "path" },
-  --         { name = "snippets" },
-  --         { name = "git" },
-  --       }),
-  --       window = {
-  --         completion = {
-  --           side_padding = 1,
-  --           winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
-  --           winblend = vim.o.pumblend,
-  --           scrolloff = 0,
-  --           col_offset = 0,
-  --           border = border("CmpBorder"),
-  --           scrollbar = true,
-  --         },
-  --         documentation = {
-  --           max_height = math.floor((WIDE_HEIGHT * 2) * (WIDE_HEIGHT / vim.o.lines)),
-  --           max_width = math.floor(WIDE_HEIGHT * (vim.o.columns / (WIDE_HEIGHT * 2 * 16 / 8))),
-  --           border = border("CmpDocBorder"),
-  --           winhighlight = "FloatBorder:NormalFloat",
-  --           winblend = vim.o.pumblend,
-  --         },
-  --       },
-  --       sorting = {
-  --         priority_weight = 2,
-  --         comparators = {
-  --           compare.sort_text,
-  --           compare.offset,
-  --           compare.exact,
-  --           compare.scopes,
-  --           compare.score,
-  --           compare.recently_used,
-  --           compare.locality,
-  --           compare.kind,
-  --           compare.length,
-  --           compare.order,
-  --         },
-  --       },
-  --     })
-  --     cmp.setup.filetype("gitcommit", {
-  --       sources = cmp.config.sources({
-  --         { name = "cmp_git" },
-  --         { name = "buffer" },
-  --       }),
-  --     })
-  --     cmp.setup.cmdline({ "/", "?" }, {
-  --       mapping = cmp.mapping.preset.cmdline(),
-  --       sources = {
-  --         { name = "buffer" },
-  --       },
-  --     })
-  --     cmp.setup.cmdline(":", {
-  --       mapping = cmp.mapping.preset.cmdline(),
-  --       sources = cmp.config.sources({
-  --         { name = "path" },
-  --         { name = "cmdline" },
-  --       }),
-  --     })
-  --   end,
-  -- },
   {
     url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     event = "LspAttach",
@@ -265,7 +61,6 @@ return {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = {
-      -- Event to trigger linters
       events = { "BufWritePost", "BufReadPost", "InsertLeave" },
       linters_by_ft = {
         fish = { "fish" },
@@ -336,12 +131,7 @@ return {
     end,
   },
   {
-    "ray-x/lsp_signature.nvim",
-    event = "LspAttach",
-    opts = {},
-  },
-  {
-    url = "https://ghp.ci/github.com/xeluxee/competitest.nvim",
+    "xeluxee/competitest.nvim",
     cmd = "CompetiTest",
     opts = {},
   },
@@ -373,7 +163,6 @@ return {
   },
   {
     "akinsho/toggleterm.nvim",
-    version = "*",
     cmd = { "ToggleTerm" },
     opts = {},
   },
@@ -382,7 +171,7 @@ return {
     cmd = { "SudaRead", "SudaWrite" },
   },
   {
-    url = "https://ghp.ci/github.com/mikavilpas/yazi.nvim",
+    "mikavilpas/yazi.nvim",
     keys = {
       {
         "<leader>fy",
@@ -413,7 +202,7 @@ return {
   },
   { "mateuszwieloch/automkdir.nvim", event = "BufWrite" },
   {
-    url = "https://ghp.ci/github.com/sontungexpt/url-open",
+    "sontungexpt/url-open",
     branch = "mini",
     event = "VeryLazy",
     config = function()
@@ -442,7 +231,6 @@ return {
   { "MeanderingProgrammer/markdown.nvim", enabled = false },
   {
     "Isrothy/neominimap.nvim",
-    version = "v3.*.*",
     lazy = false,
   },
   {
@@ -451,10 +239,7 @@ return {
     opts = {},
   },
   "nvzone/volt",
-  "nvzone/menu",
-  { "nvzone/minty", cmd = { "Shades", "Huefy" } },
   { "nvzone/timerly", cmd = "TimerlyToggle" },
-  { "nvzone/showkeys", cmd = "ShowkeysToggle", opts = { timeout = 1, maxkeys = 5 } },
   { "nvzone/typr", cmd = { "Typr", "TyprStats" } },
   {
     "yorickpeterse/nvim-window",
@@ -464,18 +249,6 @@ return {
     opts = {
       normal_hl = "Normal",
       render = "float",
-    },
-  },
-  {
-    "declancm/cinnamon.nvim",
-    version = "*", -- use latest release
-    event = "VeryLazy",
-    opts = {
-      keymaps = {
-        basic = true,
-        extra = true,
-      },
-      delay = 1,
     },
   },
 }
