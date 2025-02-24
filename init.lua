ceb1b5add36b6188d2b58362afd9ee571fb81bec
abalone <noilinux@noilinux>
2025-02-24 18:20:28 +0800

    适配 vscode-neovim

--- init.lua
+++ init.lua
@@ -1,33 +1,78 @@
 -- [nfnl] Compiled from init.fnl by https://github.com/Olical/nfnl, do not edit.
 vim.g.mapleader = " "
-local lazypath = (vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
-local nfnlpath = (vim.fn.stdpath("data") .. "/lazy/nfnl")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({git = "clone", ["--filter=blob:none"] = repo, ["--branch=stable"] = lazypath})
-else
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({"git", "clone", repo, lazypath})
 end
-if not vim.uv.fs_stat(nfnlpath) then
-  local repo = "https://github.com/Olical/nfnl.git"
-  vim.fn.system({git = "clone", [repo] = lazypath})
-else
-end
-vim.opt.rtp:prepend(nfnlpath)
 vim.opt.rtp:prepend(lazypath)
 local lazy_config = require("configs.lazy")
-do
-  local _, lazyevent = pcall(require, "lazy.core.handler.event")
-  lazyevent.mappings.LazyFile = {id = "LazyFile", event = "BufReadPost", BufWrite = "BufNewFile"}
-end
-do
-  local _, lazy = pcall(require, "lazy")
-  lazy.setup({{url = "https://github.com/Olical/nfnl", ft = "fennel"}, {import = "plugins"}}, lazy_config)
+local _, lazyevent = pcall(require, "lazy.core.handler.event")
+lazyevent.mappings.LazyFile = {
+    id = "LazyFile",
+    event = "BufReadPost",
+    BufWrite = "BufNewFile"
+}
+local _, lazy = pcall(require, "lazy")
+if not vim.g.vscode then
+    lazy.setup({{
+        import = "plugins"
+    }, {
+        "Old-Farmer/im-autoswitch.nvim",
+        event = "BufEnter",
+        opts = {
+            cmd = {
+                -- default im
+                default_im = "1",
+                -- get current im
+                get_im_cmd = "fcitx5-remote",
+                -- cmd to switch im. the plugin will put an im name in "{}"
+                -- or
+                -- cmd to switch im between active/inactive
+                switch_im_cmd = "fcitx5-remote -t"
+            }
+        }
+    }}, lazy_config)
+else
+    lazy.setup({{
+        import = "plugins-vscode"
+    }}, {
+        defaults = {
+            lazy = true,
+            version = false
+        },
+        {
+            rocks = {
+                hererocks = true -- recommended if you do not have global installation of Lua 5.1.
+            }
+        },
+        ui = {
+            icons = {
+                ft = "",
+                lazy = "󰂠 ",
+                loaded = "",
+                not_loaded = ""
+            }
+        },
+        performance = {
+            rtp = {
+                disabled_plugins = {"2html_plugin", "tohtml", "getscript", "getscriptPlugin", "gzip", "logipat",
+                                    "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers", "matchit", "tar",
+                                    "tarPlugin", "rrhelper", "spellfile_plugin", "vimball", "vimballPlugin", "zip",
+                                    "zipPlugin", "tutor", "rplugin", "syntax", "synmenu", "optwin", "compiler",
+                                    "bugreport", "ftplugin"}
+            }
+        }
+    })
 end
 require("options")
 local function _3_()
-  require("mappings")
-  return require("configs.init_funcs")
+    require("mappings")
+    return require("configs.init_funcs")
 end
-vim.schedule(_3_)
-require("autocmds")
-return nil
+if not vim.g.vscode then
+    vim.schedule(_3_)
+    require("autocmds")
+else
+    require("mappings-vscode")
+end
