b5fb785fc349f7dfcc3d3a8a70ed6a60a55b236a
Fujiwara Azusa <sizebao@gmail.com>
2026-03-04 15:22:31 +0800

    fix(surround): migrate to v4

--- lua/plugins/extras/editor/surround.lua
+++ lua/plugins/extras/editor/surround.lua
@@ -6,40 +6,33 @@ return {
 		},
 		-- version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
 		-- event = "VeryLazy",
+		-- stylua: ignore
 		keys = function(_, keys)
 			-- Populate the keys based on the user's options
 			local mappings = {
-				{ "gsa", desc = "Add Surrounding", mode = { "n", "v" } },
-				{ "gsA", desc = "Add Surrounding for this line", mode = { "n", "v" } },
-				{ "gsd", desc = "Delete Surrounding" },
-				{ "gsl", desc = "Add Surruonding(indent and enter)" },
+				{ "gsa", "<Plug>(nvim-surround-normal)", desc = "Add Surrounding" },
+				{ "gsa", "<Plug>(nvim-surround-visual)", desc = "Add Surrounding", mode = "v" },
+				{ "gsA", "<Plug>(nvim-surround-normal-cur)", desc = "Add Surrounding for this line" },
+				{ "gsd", "<Plug>(nvim-surround-delete)", desc = "Delete Surrounding" },
+				{ "gsl", "<Plug>(nvim-surround-normal-line)", desc = "Add Surrounding(indent and enter)" },
 				-- { "gsF", desc = "Find Right Surrounding" },
 				-- { "gsf", desc = "Find Left Surrounding" },
 				-- { "gsh", desc = "Highlight Surrounding" },
-				{ "gsr", desc = "Replace Surrounding" },
-				{ "gsR", desc = "Replace Surrounding(indent and enter)" },
-				{ "gs<cr>", desc = "Add Surrounding for this line(indent and enter)" },
+				{ "gsr", "<Plug>(nvim-surround-change)", desc = "Replace Surrounding" },
+				{ "gsR", "<Plug>(nvim-surround-change-line)", desc = "Replace Surrounding(indent and enter)" },
+				{ "gs<cr>", "<Plug>(nvim-surround-normal-cur-line)", desc = "Add Surrounding for this line(indent and enter)" },
+				{ "gs<cr>", "<Plug>(nvim-surround-visual-line)", desc = "Add Surrounding(indent and enter)", mode = "v" },
 			}
 			mappings = vim.tbl_filter(function(m)
 				return m[1] and #m[1] > 0
 			end, mappings)
 			return vim.list_extend(mappings, keys)
 		end,
-		opts = {
-			keymaps = {
-				-- insert = "<C-g>s",
-				-- insert_line = "<C-g>S",
-				normal = "gsa",
-				normal_cur = "gsA",
-				normal_line = "gsl",
-				normal_cur_line = "gs<cr>",
-				visual = "gsa",
-				visual_line = "gsA",
-				delete = "gsd",
-				change = "gsr",
-				change_line = "gsR",
-			},
-		},
+		opts = {},
+		config = function(_, opts)
+			require("nvim-surround").setup(opts)
+			vim.g.nvim_surround_no_mappings = true
+		end,
 	},
 	-- {
 	-- 	"chrisgrieser/nvim-various-textobjs",
