fde66046850e606f4b539336c8aadaf46c122687
fujiwara <noilinux@true>
2026-08-07 07:30:29 +0800

    fix(fyler): fix icons, fit with new format

--- lua/plugins/extras/editor/fyler.lua
+++ lua/plugins/extras/editor/fyler.lua
@@ -1,78 +1,62 @@
 return {
-	"A7Lavinraj/fyler.nvim",
-	dependencies = { "nvim-mini/mini.icons" },
-	opts = {
-		hooks = {
-			on_rename = function(from, to)
-				if Abalone.lazy.has("snacks.nvim") then
-					Snacks.rename.on_rename_file(from, to)
-				end
-			end,
-		},
-		views = {
-			finder = {
-				delete_to_trash = true,
-				close_on_select = false,
-				confirm_simple = true,
-				default_explorer = true,
-				watcher = {
-					enabled = true,
-				},
-				indentscope = {
-					enabled = not Abalone.lazy.has("blink.indent"),
-					group = "FylerIndentMarker",
-					markers = "│",
-				},
-				win = {
-					win_opts = {
-						concealcursor = "nvic",
-						conceallevel = 3,
-						cursorline = true,
-						cursorlineopt = "both",
-						number = true,
-						foldcolumn = "0",
-						relativenumber = true,
-						winhighlight = "Normal:FylerNormal,NormalNC:FylerNormalNC",
-						wrap = false,
-						sidescrolloff = 0,
-					},
-					kinds = {
-						split_right = {
-							width = "20%",
-						},
-					},
-				},
-			},
-		},
-	},
-	keys = {
-		{
-			"<leader>fp",
-			function()
-				require("fyler").toggle({
-					dir = Abalone.root.get(), -- (Optional) Start in specific directory
-					kind = "split_right", -- (Optional) Use custom window layout
-				})
-				vim.defer_fn(function()
-					vim.cmd("normal! 0")
-				end, 500)
-			end,
-			desc = "Toggle Fyler (Root dir)",
-		},
-		{
-			"<leader>fP",
-			function()
-				require("fyler").toggle({
-					dir = vim.uv.cwd(),
-					kind = "split_right",
-				})
-				vim.defer_fn(function()
-					vim.cmd("normal! 0")
-				end, 500)
-			end,
-			desc = "Toggle Fyler (cwd)",
-		},
-		{ "<leader>e", "<leader>fp", desc = "Toggle Fyler (root dir)", remap = true },
-		{ "<leader>E", "<leader>fP", desc = "Toggle Fyler (cwd)", remap = true },
-	},
+    "A7Lavinraj/fyler.nvim",
+    dependencies = { "nvim-mini/mini.icons" },
+    opts = {
+        auto_confirm_simple_mutation = true,
+        bound_cursor = true,
+        follow_current_file = true,
+        kind = 'split_right',
+        ui = {
+            -- Whether to draw indent guides at each depth level.
+            hidden_items = {
+                -- Toggleable pre-defined switches (e.g. 'dotfiles' to hide files starting with a dot).
+                switches = { 'dotfiles' },
+                -- Toggleable patterns (Lua patterns matched against the full path).
+                patterns = {},
+                -- Always visible items matching these patterns, even if they would normally be hidden.
+                always_visible = {},
+                -- Always hide items matching these patterns, even if they would normally be visible.
+                always_hidden = {},
+            },
+            indent_guides = true,
+        },
+        integrations = { icon = 'mini_icons' },
+        hooks = {
+            on_rename = function(from, to)
+                if Abalone.lazy.has("snacks.nvim") then
+                    Snacks.rename.on_rename_file(from, to)
+                end
+            end,
+        },
+    },
+    keys = {
+        {
+            "<leader>fp",
+            function()
+                require("fyler").toggle({
+                    dir = Abalone.root.get(), -- (Optional) Start in specific directory
+                    kind = "split_right",     -- (Optional) Use custom window layout
+                })
+                vim.defer_fn(function()
+                    vim.cmd("normal! 0")
+                end, 500)
+            end,
+            desc = "Toggle Fyler (Root dir)",
+        },
+        {
+            "<leader>fP",
+            function()
+                require("fyler").toggle({
+                    dir = vim.uv.cwd(),
+                    kind = "split_right",
+                })
+                vim.defer_fn(function()
+                    vim.cmd("normal! 0")
+                end, 500)
+            end,
+            desc = "Toggle Fyler (cwd)",
+        },
+        { "<leader>e", "<leader>fp", desc = "Toggle Fyler (root dir)", remap = true },
+        { "<leader>E", "<leader>fP", desc = "Toggle Fyler (cwd)",      remap = true },
+    },
 }
--- lua/plugins/extras/ui/edgy.lua
+++ lua/plugins/extras/ui/edgy.lua
@@ -64,7 +64,7 @@ return {
 				},
 				right = {
 					{ title = "Grug Far", ft = "grug-far", size = { width = 0.4 } },
-					{ title = "Project", ft = "fyler", size = { width = 0.20 } },
+					{ title = "Project", ft = "fyler_finder", size = { width = 0.20 } },
 				},
 				keys = {
 					-- increase width
